202262e1986b6c17765bdfedeca2fff5cccdcd45
mariappan.sivakumar@zucisystems.com <148471944+Mariappan-Sivakumar-ZS0528@users.noreply.github.com>
2024-03-20 12:14:38 +0530

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "http://localhost:8081/getUserUtterance";
+const rasa_server_url = "http://192.168.14.69:8081/getUserUtterance";
 const sender_id = uuidv4();
